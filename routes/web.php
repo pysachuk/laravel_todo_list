b6ce31d147ae292f5ada4f77da78a2bac45890c2
pysachuk <palalamm@gmail.com>
2021-09-14 21:33:04 +0300

    Add login authentification

--- routes/web.php
+++ routes/web.php
@@ -12,15 +12,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/tasks', [\App\Http\Controllers\TaskController::class, 'index'])
-    ->name('tasks');
-Route::post('/task/store', [\App\Http\Controllers\TaskController::class, 'store'])
-    ->name('task.store');
-Route::get('/task/complete/{task}', [\App\Http\Controllers\TaskController::class, 'complete'])
-    ->name('task.complete');
-Route::delete('task/delete/{task}', [\App\Http\Controllers\TaskController::class, 'delete'])
-    ->name('task.delete');
+Route::middleware('auth') -> group(function(){
+    Route::get('/tasks', [\App\Http\Controllers\TaskController::class, 'index'])
+        ->name('tasks');
+    Route::post('/task/store', [\App\Http\Controllers\TaskController::class, 'store'])
+        ->name('task.store');
+    Route::get('/task/complete/{task}', [\App\Http\Controllers\TaskController::class, 'complete'])
+        ->name('task.complete');
+    Route::delete('task/delete/{task}', [\App\Http\Controllers\TaskController::class, 'delete'])
+        ->name('task.delete');
+});
 
 Auth::routes();
 
